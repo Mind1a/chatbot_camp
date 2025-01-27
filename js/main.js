f8787c1ae8a1589f7c76d019377ce2f3299d229e
Giorgi Kheladze <giorgikheladze546@gmail.com>
2025-01-28 03:54:24 +0400

    update chat-box
    1.removing hovers
    2.fixing header & footer

--- js/main.js
+++ js/main.js
@@ -1,42 +1,23 @@
 const iframe = document.getElementById("chatbotIframe");
+const mainDiv = document.getElementById("message-btn");
 const open = document.getElementById("open");
 const close = document.getElementById("close");
 const start = document.getElementById("start");
-const fixColor1 = document.getElementById("message-btn");
-const fixColor2 = document.getElementById("start");
 
-open.addEventListener("click", () => {
-    iframe.style.display = "block";
-    open.style.display = "none";
-    close.style.display = "block";
-    fixColor1.classList.add("red");
-    fixColor2.classList.add("red");
-    start.innerText = "ჩატის გამორთვა";
-});
-
-start.addEventListener("click", () => {
+// ფუნქცია რომ ავირიდოთ კოდის დუბლიკაცია
+function change(){
     if (iframe.style.display === "block") {
         iframe.style.display = "none";
         open.style.display = "block";
         close.style.display = "none";
-        fixColor1.classList.remove("red");
-        fixColor2.classList.remove("red");
         start.innerText = "ჩატის დაწყება";
     } else {
         iframe.style.display = "block";
         open.style.display = "none";
         close.style.display = "block";
-        fixColor1.classList.add("red");
-        fixColor2.classList.add("red");
         start.innerText = "ჩატის გამორთვა";
     }
-});
+}
 
-close.addEventListener("click", ()=>{
-    iframe.style.display = "none";
-    open.style.display = "block";
-    close.style.display = "none";
-    fixColor1.classList.remove("red");
-    fixColor2.classList.remove("red");
-    start.innerText = "ჩატის დაწყება";
-})
+mainDiv.addEventListener("click", change);
+start.addEventListener("click", change);
